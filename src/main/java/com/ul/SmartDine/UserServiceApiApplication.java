8078966f3cb9d644aff302fbaa5688597ae31916
udara885 <ulakshan061@gmail.com>
2026-08-24 00:42:41 +0530

    init

---
+++ src/main/java/com/ul/SmartDine/UserServiceApiApplication.java
@@ -0,0 +1,15 @@
+package com.ul.SmartDine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ul/SmartDine/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ul.SmartDine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
