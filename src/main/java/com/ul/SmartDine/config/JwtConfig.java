8b813064a3d2fa033d7bf3a459ff7835518b5941
Udara Lakshan <ulakshan061@gmail.com>
2026-09-02 01:17:45 +0530

    17

--- src/main/java/com/ul/SmartDine/config/JwtConfig.java
+++ src/main/java/com/ul/SmartDine/config/JwtConfig.java
@@ -1,4 +1,20 @@
 package com.ul.SmartDine.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtConfig.JwtProperties.class)
 public class JwtConfig {
+    @Getter
+    @Setter
+    @ConfigurationProperties(prefix = "jwt")
+    public static class JwtProperties {
+        private String secret;
+        private long expirationMs = 36000000L;
+        private long refreshExpirationMs = 72000000L;
+    }
 }
